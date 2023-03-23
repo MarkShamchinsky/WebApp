93e285f53025a711a9d77eac2f9530e0c5d2d590
MarkShamchinsky <volooo@list.ru>
2023-03-23 07:31:08 +0300

    start server and db

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module WebApp
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.9.0
+require (
+	github.com/gin-gonic/gin v1.9.0
+	github.com/lib/pq v1.10.7
+)
 
 require (
 	github.com/bytedance/sonic v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+var router *gin.Engine
+var connectionString = "host = 127.0.0.1 port=5432 user = postgres password = postgres dbname  = WebAppDB sslmode = disable"
+var connection *sql.DB
 
 func main() {
-	gin.Default()
+	var e error
+	connection, e = sql.Open("postgres", connectionString)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
 
+	router = gin.Default()
+	router.LoadHTMLGlob("html/*.html")
+	router.GET("/", HandlerIndex)
+
+	_ = router.Run("127.0.0.1:8080")
 }
 
+func HandlerIndex(c *gin.Context) {
 
+	rows, e := connection.Query(`SELECT "Name" FROM "User" WHERE "Login"=$1 AND "Password"=$2`, "admin", "admin")
+	if e != nil {
+		fmt.Println(e)
+		c.HTML(400, "400.html", gin.H{
+			"Error": e.Error(),
+		})
+		return
+	}
+
+	var name string
+
+	for rows.Next() {
+		e = rows.Scan(&name)
+		if e != nil {
+			fmt.Println(e)
+			c.HTML(400, "400.html", gin.H{
+				"Error": e.Error(),
+			})
+			return
+		}
+	}
+
+	c.HTML(200, "index.html", gin.H{
+		"Name": name,
+	})
+}
